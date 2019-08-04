91cb67ab30cf1df3f9adc2c1c09b65326d6939d3
Ana M Grijalba <happypillusa@outlook.com>
2019-08-04 12:23:08 -0400

    Update CheckBalance.js
    using conditionals .One "if" and 3 "else if

--- CheckBalance.js
+++ CheckBalance.js
@@ -1,24 +1,26 @@
-/*
- * Checking Your Balance
+ /*
+ * Programming Quiz - Checking Your Balance (3-5)-UDACITY IPND-
  */
 
 // change the values of `balance`, `checkBalance`, and `isActive` to test your code
-var balance = 0; //account balance
-var checkBalance = true; //if you want to check balance
-var isActive = true; //if account is active
+var balance = 325.00;
+var checkBalance = true;
+var isActive = false;
 
-// your code goes here
-//console.log('Do you want to check your balance?');
-if (checkBalance) {
-    if (isActive && balance > 0) {
-        console.log(`Your balance is $${balance}.`);
-    } else if (!isActive) {
-        console.log("Your account is no longer active.");
-    } else if (balance === 0) {
-        console.log("Your account is empty.");
-    } else {
-        console.log("Your balance is negative. Please contact bank.");
-    }
-} else {
-    console.log("Thank you. Have a nice day!");
+	if (!checkBalance){
+		console.log("Thanks you. Have a nice day!");
+	}
+else if ( isActive && balance > 0) {
+	console.log("Your balance is $" + balance.toFixed(2) + ".");
 }
+else if (!isActive) {
+	console.log("Your account is no longer active.");
+}
+else if ( isActive  && balance === 0){
+	console.log("Your account is empty.");
+}
+
+else if ( isActive && balance !===0){
+	console.log("Your balance is negative, please contact Your Bank.");
+}
+
